b7ec82f56546bb958dd923a40a1b4e8da8147cba
Jan Ritter <git@janrtr.de>
2019-05-05 17:48:25 +0200

    feat: added support for linux and mac

--- generateFormula.py
+++ generateFormula.py
@@ -3,7 +3,8 @@ from jinja2 import Environment, FileSystemLoader
 import os
 
 envVersion = os.environ['VERSION']
-envFileHash = os.environ['FILE_HASH']
+envFileHashMac = os.environ['FILE_HASH_MAC']
+envFileHashLinux = os.environ['FILE_HASH_LINUX']
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
@@ -11,4 +12,4 @@ env = Environment(loader=file_loader)
 template = env.get_template('terrastate.rb.j2')
 
 output = template.stream(
-    version=envVersion, sha256=envFileHash).dump('terrastate.rb')
+    version=envVersion, sha256Mac=envFileHashMac, sha256Linux=envFileHashLinux).dump('terrastate.rb')
--- terrastate.rb
+++ terrastate.rb
@@ -3,12 +3,22 @@
 
 class Terrastate < Formula
     desc "Tool to manage multiple state backends in Terraform - Allows Multi account setups"
-    url "https://github.com/janritter/terrastate/releases/download/1.2.0/darwin_amd64_terrastate"
-    sha256 "fe22095fffbcc177a9184c815f169fcc5e8e0511f622d7e669e30a8333ca92a5"
-  
+    if OS.mac?
+      url "https://github.com/janritter/terrastate/releases/download/1.2.0/darwin_amd64_terrastate"
+      sha256 "fe22095fffbcc177a9184c815f169fcc5e8e0511f622d7e669e30a8333ca92a5"
+    elsif OS.linux?
+      url "https://github.com/janritter/terrastate/releases/download/1.2.0/linux_amd64_terrastate"
+      sha256 "f88dadb4b950350ce458a5a52ed3c2e315219923aa5fea0480a7841f597995d7"
+    end
+
     def install
-      bin.install "darwin_amd64_terrastate"
-      mv bin/"darwin_amd64_terrastate", bin/"terrastate"
+      if OS.mac?
+        bin.install "darwin_amd64_terrastate"
+        mv bin/"darwin_amd64_terrastate", bin/"terrastate"
+      elsif OS.linux?
+        bin.install "linux_amd64_terrastate"
+        mv bin/"linux_amd64_terrastate", bin/"terrastate"
+      end
     end
   
     test do
