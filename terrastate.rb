e0480851f8751fc1188cd2cabb98d3583acf8a21
Jan Ritter <git@janrtr.de>
2019-05-05 16:53:54 +0200

    fix: removed invalid version key

--- terrastate.rb
+++ terrastate.rb
@@ -5,7 +5,6 @@ class Terrastate < Formula
     desc "Tool to manage multiple state backends in Terraform - Allows Multi account setups"
     url "https://github.com/janritter/terrastate/releases/download/1.2.0/darwin_amd64_terrastate"
     sha256 "fe22095fffbcc177a9184c815f169fcc5e8e0511f622d7e669e30a8333ca92a5"
-    version 1.2.0
   
     def install
       bin.install "darwin_amd64_terrastate"
