80eee0769314c04b6a40c23d179abb8b5ca82b2f
Travis CI User <travis@example.org>
2019-05-05 15:54:59 +0000

    chore: deployment of version 1.2.0 - 2019-05-05_15:54:07

--- terrastate.rb
+++ terrastate.rb
@@ -5,10 +5,10 @@ class Terrastate < Formula
     desc "Tool to manage multiple state backends in Terraform - Allows Multi account setups"
     if OS.mac?
       url "https://github.com/janritter/terrastate/releases/download/1.2.0/darwin_amd64_terrastate"
-      sha256 "fe22095fffbcc177a9184c815f169fcc5e8e0511f622d7e669e30a8333ca92a5"
+      sha256 "ab5dbf3c3acb614fcbc7d7ca0be7125a82e1555aad8f78dceb9926d55507f70d"
     elsif OS.linux?
       url "https://github.com/janritter/terrastate/releases/download/1.2.0/linux_amd64_terrastate"
-      sha256 "f88dadb4b950350ce458a5a52ed3c2e315219923aa5fea0480a7841f597995d7"
+      sha256 "00df23b18151f6dfbbbe6cefa8f61f28ba02dd997f0940800a9ebc506807a202"
     end
 
     def install
