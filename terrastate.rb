4df649c14ae14476d74f340e76857494b1de307f
CircleCI <git+circleci@janrtr.de>
2020-08-24 19:44:56 +0000

    chore: deployment of version 1.6.1 -

--- terrastate.rb
+++ terrastate.rb
@@ -4,13 +4,13 @@
 class Terrastate < Formula
     desc "Tool to manage multiple state backends in Terraform - Allows Multi account setups"
     if OS.mac?
-      url "https://github.com/janritter/terrastate/releases/download/1.6.0/darwin_amd64_terrastate"
-      sha256 "d5c5a3768eb4b2e3d66e6eb7ba3fcbca66b968caef69f93242082bb5f972b6af"
-      version "1.6.0"
+      url "https://github.com/janritter/terrastate/releases/download/1.6.1/darwin_amd64_terrastate"
+      sha256 "d34118ffddaa52ca174fa1f2aa7d8e444c647a02e2189c9980fa96f94ed99ad5"
+      version "1.6.1"
     elsif OS.linux?
-      url "https://github.com/janritter/terrastate/releases/download/1.6.0/linux_amd64_terrastate"
-      sha256 "805913a649323246ee2c84c3ec4fcb5ca64abf7d6d7b342c1951b6728f16b9cb"
-      version "1.6.0"
+      url "https://github.com/janritter/terrastate/releases/download/1.6.1/linux_amd64_terrastate"
+      sha256 "5331286c9870f899b321294b5a2f56248859da3aaf800cc7216aab115b53eaa0"
+      version "1.6.1"
     end
 
     def install
