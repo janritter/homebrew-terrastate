9fdeb35ed5ee53a6e88b48bc9b4d533756cd2648
CircleCI <git+circleci@janrtr.de>
2021-09-01 19:27:07 +0000

    chore: deployment of version 1.9.0 -

--- terrastate.rb
+++ terrastate.rb
@@ -5,11 +5,11 @@ class Terrastate < Formula
     desc "Tool to manage multiple state backends in Terraform - Allows Multi account setups"
     if OS.mac?
       url "https://github.com/janritter/terrastate/releases/download/1.9.0/darwin_amd64_terrastate"
-      sha256 "74bba0de4a01825b114525b9e7284d07516038d963e587f405369f76101f9c59"
+      sha256 "698efa747a879306a33487db7956a32480ef722310fe613e86db7bab8ffce65a"
       version "1.9.0"
     elsif OS.linux?
       url "https://github.com/janritter/terrastate/releases/download/1.9.0/linux_amd64_terrastate"
-      sha256 "bccb58150cda9aabc3a03957b9a0ede1337f8d962bd3a6c7cf5884a1074be553"
+      sha256 "6d3a1552ad0708541d98657b408116cb98cfd7fb54d8ea062a741874e3752083"
       version "1.9.0"
     end
 
