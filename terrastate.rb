ff8591417298f796a57f507a45add3ecccb7e97b
CircleCI <git+circleci@janrtr.de>
2022-06-21 19:18:40 +0000

    chore: deployment of version 1.11.1 -

--- terrastate.rb
+++ terrastate.rb
@@ -5,11 +5,11 @@ class Terrastate < Formula
     desc "Tool to manage multiple state backends in Terraform - Allows Multi account setups"
     if OS.mac?
       url "https://github.com/janritter/terrastate/releases/download/1.11.1/darwin_amd64_terrastate"
-      sha256 "6da562c04cd8e99bd50d87bddddae0fff444ac69fe1c8008ee64678d5524a92c"
+      sha256 "e8272710f67bb9ea988ece976bbce4eaf6e54329983c82a4e07aa3dee3e6149d"
       version "1.11.1"
     elsif OS.linux?
       url "https://github.com/janritter/terrastate/releases/download/1.11.1/linux_amd64_terrastate"
-      sha256 "860654806588f4c9a91ddb255249ff658eaacbbc7a398f59bedb4fdb9308534c"
+      sha256 "843a7e17e7bbaa8e577d6b4825bec7aa7a6ff7d8b9fcabc5525fd5b37135b4c2"
       version "1.11.1"
     end
 
